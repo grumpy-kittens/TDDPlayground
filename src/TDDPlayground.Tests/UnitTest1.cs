261ad96f3a04a3578474579c15e48cc3c4b996fb
Dmytro Kavchuk <kavchuk.dmytro@gmail.com>
2020-10-02 18:35:31 +0300

    Initial setup

---
+++ src/TDDPlayground.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace TDDPlayground.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
